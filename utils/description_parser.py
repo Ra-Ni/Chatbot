b0c31675632efc73a42180f32a8255289fdb9ae3
Ra-Ni <ranii.rafid@gmail.com>
2020-03-03 22:43:21 -0500

    small tweaks (no need for file hard coding, we will merge this later on

--- utils/description_parser.py
+++ utils/description_parser.py
@@ -1,38 +1,23 @@
-import rdflib
 import json
 import re
-import os
 
-def parse_descriptions():
 
-    #specifying path to assets directory
-    asset_path = os.path.join(os.path.dirname(__file__), '..', 'assets')
+def parse_descriptions(target, output):
+    with open(target, 'rb') as file:
+        raw_data = json.load(file)
 
-    with open(os.path.join(asset_path, 'description.json'), 'rb') as file:
-        RawData = json.load(file)
-        
         # establish and write prefix URIs
-        out = open(os.path.join(asset_path, 'CourseDescriptions.txt'), "w")
-        out.write (
-            "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>."
-        ) 
-        
+        out = open(output, "w")
+        out.write("@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>.")
+
         # Creating tuples
-        for Course in RawData:
-            print ("Parsing: " + Course["ID"])
-            
-            description = re.sub("Prerequisite.*\.", "", Course["description"])  # remove all prereqs from the description
+        for course in raw_data:
+            print("Parsing: " + course["ID"])
+
+            description = re.sub("Prerequisite.*\.", "",
+                                 course["description"])  # remove all prereqs from the description
             description = re.sub("NOTE|Note:.*\.", "", description)  # The notes in the description are useless too
-            
+            description = description.replace('\n', '')
+
             if description != "":
-                tuples = (
-                    "\n\nCourse:" + Course["ID"] + 
-                    "\n\t" + "rdfs:comment " + "\"" + description.replace("\n", "") + "\"."
-                    )
-                out.write(tuples)
-        
-
-#Main method begins here
-if __name__ == "__main__":
-
-    parse_descriptions()
+                out.write(f'\n\nCourse:{course["ID"]}\n\trdfs:comment \"{description}\".')
