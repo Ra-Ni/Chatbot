8e279764450bed01d522455bd0c825a0276cb132
Ra-Ni <ranii.rafid@gmail.com>
2020-03-03 22:49:09 -0500

    used recursive with

--- utils/description_parser.py
+++ utils/description_parser.py
@@ -3,21 +3,22 @@ import re
 
 
 def parse_descriptions(target, output):
-    with open(target, 'rb') as file:
-        raw_data = json.load(file)
+    with open(target, 'rb') as reader:
+        raw_data = json.load(reader)
 
         # establish and write prefix URIs
-        out = open(output, "w")
-        out.write("@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>.")
+        with open(output, 'w') as writer:
+            writer.write("@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>.")
 
-        # Creating tuples
-        for course in raw_data:
-            print("Parsing: " + course["ID"])
+            # Creating tuples
+            for course in raw_data:
+                print(f"Parsing: {course['ID']}")
 
-            description = re.sub("Prerequisite.*\.", "",
-                                 course["description"])  # remove all prereqs from the description
-            description = re.sub("NOTE|Note:.*\.", "", description)  # The notes in the description are useless too
-            description = description.replace('\n', '')
+                # remove all prereqs from the description
+                description = re.sub("Prerequisite.*\.", "", course["description"])
+                # The notes in the description are useless too
+                description = re.sub("NOTE|Note:.*\.", "", description)
+                description = description.replace('\n', '')
 
-            if description != "":
-                out.write(f'\n\nCourse:{course["ID"]}\n\trdfs:comment \"{description}\".')
+                if description != "":
+                    writer.write(f'\n\nCourse:{course["ID"]}\n\trdfs:comment \"{description}\".')
